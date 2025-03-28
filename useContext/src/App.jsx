dc9a46460a2b41cda00c1d912a19bf2e1cd141d8
PabloObando <pabloobando03@gmail.com>
2025-03-28 14:37:11 -0600

    commit

--- useContext/src/App.jsx
+++ useContext/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, useContext, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Crear el contexto
+const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+function ChildComponent() {
+  const { theme, setTheme } = useContext(ThemeContext);
+  return (
+    <div>
+      <p>Que es lo que vez: {theme}</p>
+      <button onClick={() => setTheme(theme === "anime" ? "pelicula" : "anime")}>
+        Cambiar Tema
+      </button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <ThemeProvider>
+      <h1>useContext para temas</h1>
+      <ChildComponent />
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
